07ff2d3fd9f62a1a583de0c329b3f975e620180c
Alex Lemeshchuk <alexyskr@gmail.com>
2021-01-19 20:00:37 +0200

    Updates

---
+++ precache-manifest.d84b46eea08a66675d6a320728373ea3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19dd490e43f67c9221cd010b9fc2cb92",
+    "url": "./index.html"
+  },
+  {
+    "revision": "757defa2473fd0b13159",
+    "url": "./static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "c77cb08b53ca392d5172",
+    "url": "./static/css/main.33aa49d0.chunk.css"
+  },
+  {
+    "revision": "757defa2473fd0b13159",
+    "url": "./static/js/2.1f9f5d05.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.1f9f5d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c77cb08b53ca392d5172",
+    "url": "./static/js/main.539e6068.chunk.js"
+  },
+  {
+    "revision": "00722c5035a6ce4d8014",
+    "url": "./static/js/runtime-main.0b6bfc7e.js"
+  }
+]);
